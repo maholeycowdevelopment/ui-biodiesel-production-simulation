6b04d97c30a9ad260582fdcb20a1db962fb7f981
Patrick VanVorce <maholeycowdevelopment@gmail.com>
2018-04-21 11:16:02 -0700

    Added interface to BiodieselStudio.tsx

--- biodieselApp/ClientApp/components/BiodieselStudio.tsx
+++ biodieselApp/ClientApp/components/BiodieselStudio.tsx
@@ -1,9 +1,31 @@
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
+import { NextButton } from './shared/NextButton'
+
+interface StationState {
+    station: string[];
+    stationNumber: number;
+}
 
 export class BiodieselStudio extends React.Component<RouteComponentProps<{}>> {
+    /*constructor(props) {
+        super(props);
+
+        this.state = {
+            station = ["StationZero", "StationOne", "StationTwo",
+                        "StationThree", "StationFour", "StationFive",
+                        "StationSix", "StationSeven"],
+            stationNumber = 0
+        };
+    }*/
 
     public render() {
-        return <h1>BIODIESEL STUDIO</h1>
+        return (
+            <div>
+                <h1>BIODIESEL STUDIO</h1>
+                <NextButton />
+            </div>
+        );
     }
+
 }
