1d134a0ee2e000e3d7db36137b65b4218dc0285b
Patrick VanVorce <maholeycowdevelopment@gmail.com>
2018-04-23 07:52:53 -0700

    Added more info to home page, removed hard to see gold color on links

--- biodieselApp/ClientApp/components/Home.tsx
+++ biodieselApp/ClientApp/components/Home.tsx
@@ -13,6 +13,19 @@ export class Home extends React.Component<RouteComponentProps<{}>, {}> {
                     of Idaho! If you haven't been here before, you might want to check out
                     the Tutorial section. Otherwise, go ahead and jump into the biodiesel
                     studio. </p>
+                    <p className="paragraph">Here are some reasons to use biodiesel:</p>
+                    <ul>
+                        <li>Renewable</li>
+                        <li>Higher Cetane Number</li>
+                        <li>Cleaner Burning</li>
+                        <li>Nontoxic</li>
+                        <li>Environmentally Friendly</li>
+                        <li>Domestic</li>
+                        <li>More food</li>
+                        <li>Adds to fuel lubricity</li>
+                        <li>Interchangeability</li>
+                    </ul>
+                    <p className="paragraph">Want the specifics? Go check out: <a href='http://biodieseleducation.org/Use/BiodieselBenefits.html'>Biodiesel Benefits</a></p>
                 </div>
                 <div className="col-sm-6">
                     <img className="home-image" src={require('./img/Administration_Building.jpg')} alt="Admin Building Picture" />
@@ -27,8 +40,8 @@ export class Home extends React.Component<RouteComponentProps<{}>, {}> {
                     National Biodiesel Education Program and the degrees offered at the
                     University of Idaho that help innovators enter this exciting field!</p>
                     <ul>
-                        <li><a className="bullet-point" href='http://biodieseleducation.org/index.html'>National Biodiesel Education Program</a></li>
-                        <li><a className="bullet-point" href='http://www.uidaho.edu/academics'>University of Idaho Academics</a></li>
+                        <li><a href='http://biodieseleducation.org/index.html'>National Biodiesel Education Program</a></li>
+                        <li><a href='http://www.uidaho.edu/academics'>University of Idaho Academics</a></li>
                     </ul>
                 </div>
             </div>
